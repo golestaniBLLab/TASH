e24fc208a9ba8c31d965bb31baf5d84d1252a453
Giulio Degano <gDegano@users.noreply.github.com>
2023-10-12 17:32:49 +0200

    changing selection of only folders

--- TASH_MCAI_Complete/TASH_DefineSubjects.m
+++ TASH_MCAI_Complete/TASH_DefineSubjects.m
@@ -3,9 +3,21 @@ function [sub, Dload, Dsave] = TASH_DefineSubjects
 
 Dload = getenv('SUBJECTS_DIR');
 listing = dir(Dload);
-sub = {listing(3:end).name};
+
+ 
+
+% Get a logical vector that tells which is a directory.
+dirFlags = [listing.isdir];
+% Extract only those that are directories.
+subFolders = listing(dirFlags); % A structure with extra info.
+
+ 
+
+sub = {subFolders(3:end).name};
 Index = find(contains(sub,'fsaverage'));
 sub(Index)=[];
+
+ 
 
 Dsave = getenv('TASH_DIR');
 
